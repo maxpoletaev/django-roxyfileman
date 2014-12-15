289bd575d372089bbf1d94aa3748440defa02981
Maxim Poletaev <zenwalker2@gmail.com>
2014-12-15 12:01:52 +0300

    Only for staff users

--- roxyfileman/urls.py
+++ roxyfileman/urls.py
@@ -1,21 +1,22 @@
+from django.contrib.admin.views.decorators import staff_member_required
 from django.conf.urls import url, patterns
 from roxyfileman import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.index),
-    url(r'conf\.json', views.conf),
-    url(r'dirlist/', views.dirlist),
-    url(r'fileslist/', views.fileslist),
-    url(r'createdir/', views.createdir),
-    url(r'deletedir/', views.deletedir),
-    url(r'movedir/', views.movedir),
-    url(r'renamedir/', views.renamedir),
-    url(r'copydir/', views.copydir),
-    url(r'renamefile/', views.renamefile),
-    url(r'movefile/', views.movefile),
-    url(r'copyfile/', views.copyfile),
-    url(r'deletefile/', views.deletefile),
-    url(r'upload/', views.upload),
-    url(r'thumb/', views.thumb),
+    url(r'^$', staff_member_required(views.index)),
+    url(r'conf\.json', staff_member_required(views.conf)),
+    url(r'dirlist/', staff_member_required(views.dirlist)),
+    url(r'fileslist/', staff_member_required(views.fileslist)),
+    url(r'createdir/', staff_member_required(views.createdir)),
+    url(r'deletedir/', staff_member_required(views.deletedir)),
+    url(r'movedir/', staff_member_required(views.movedir)),
+    url(r'renamedir/', staff_member_required(views.renamedir)),
+    url(r'copydir/', staff_member_required(views.copydir)),
+    url(r'renamefile/', staff_member_required(views.renamefile)),
+    url(r'movefile/', staff_member_required(views.movefile)),
+    url(r'copyfile/', staff_member_required(views.copyfile)),
+    url(r'deletefile/', staff_member_required(views.deletefile)),
+    url(r'upload/', staff_member_required(views.upload)),
+    url(r'thumb/', staff_member_required(views.thumb)),
 )
