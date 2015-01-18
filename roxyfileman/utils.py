f24e700d4d483f9adc03d009989f252e3aed1138
Maxim Poletaev <zenwalker2@gmail.com>
2015-01-19 00:30:51 +0300

    Update utils.py

--- roxyfileman/utils.py
+++ roxyfileman/utils.py
@@ -4,8 +4,6 @@ from django.core.files.base import ContentFile
 from django.conf import settings
 import os, time, uuid, json, re
 
-media_storage = FileSystemStorage(location=settings.MEDIA_ROOT)
-
 
 def json_response(data, success=True):
     Response = HttpResponse if success else HttpResponseBadRequest
@@ -33,6 +31,7 @@ def safepath(path, *paths):
 class Upload:
     def __init__(self, uploaded_file):
         self.raw_name, self.ext = os.path.splitext(uploaded_file.name)
+        self.storage = FileSystemStorage(location=settings.MEDIA_ROOT)
         self.name = self.build_name(self.raw_name)
         self.file = uploaded_file
 
@@ -40,7 +39,7 @@ class Upload:
         self.create_dir(os.path.join(settings.MEDIA_ROOT, dir))
 
         path = os.path.join(dir, self.name + self.ext)
-        media_storage.save(path, ContentFile(self.file.read()))
+        self.storage.save(path, ContentFile(self.file.read()))
         return os.path.join(os.path.dirname(settings.MEDIA_ROOT), path)
 
     @staticmethod
