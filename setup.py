483ea054601106274ad3b6f0112c04308e294728
Maxim Poletaev <zenwalker2@gmail.com>
2015-04-04 11:05:26 +0300

    Up version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-roxyfileman',
-    version='1.4.2.2',
+    version='1.4.2.3',
     packages=find_packages(),
     include_package_data=True,
     description='Integrate Django with Roxy Fileman',
