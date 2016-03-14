eede6e70b865a5a191c3699a37e2e40229b68e30
Maxim Poletaev <max.poletaev@gmail.com>
2016-03-14 18:51:00 +0300

    Bump version, change versioning strategy

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-roxyfileman',
-    version='1.4.2.5',
+    version='1.142.3',
     packages=find_packages(),
     include_package_data=True,
     description='Integrate Django with Roxy Fileman',
     author='Maxim Poletaev',
-    author_email='zenwalker2@gmail.com',
+    author_email='max.poletaev@gmail.com',
     url='https://github.com/zenwalker/django-roxyfileman',
     keywords=['filemanager'],
     license='GPLv3',
@@ -16,5 +16,8 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
     ]
 )
