3ae9e9fba2f13537c8ea8c7e2f8b92a25431b8e9
Vadim <vadiqwe@yandex.ru>
2016-05-17 11:11:11 +0400

    Update main.js

--- roxyfileman/static/roxyfileman/js/main.js
+++ roxyfileman/static/roxyfileman/js/main.js
@@ -836,3 +836,22 @@ function setFile(){
     break;
   }
 }
+function FileSelected(file){
+  /**
+   * file is an object containing following properties:
+   *
+   * fullPath - path to the file - absolute from your site root
+   * path - directory in which the file is located - absolute from your site root
+   * size - size of the file in bytes
+   * time - timestamo of last modification
+   * name - file name
+   * ext - file extension
+   * width - if the file is image, this will be the width of the original image, 0 otherwise
+   * height - if the file is image, this will be the height of the original image, 0 otherwise
+   *
+   */
+  // Get the ID of the input to fill
+  var fieldId = RoxyUtils.GetUrlParam('txtFieldId');
+  $(window.parent.document).find('#' + fieldId).attr('value', file.fullPath);
+  window.parent.closeCustomRoxy2();
+}
